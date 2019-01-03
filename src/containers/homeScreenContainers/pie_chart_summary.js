cc03f372127094222591d2eca16cd8303b2236c3
EricTsai <etsai@PowerUsers-MBP.attlocal.net>
2019-01-03 00:21:27 -0800

    adding api example code

--- src/containers/homeScreenContainers/pie_chart_summary.js
+++ src/containers/homeScreenContainers/pie_chart_summary.js
@@ -11,6 +11,25 @@ class PieChartSummary extends Component {
         super(props);
     }
 
+    // TODO: Test the api call with redux
+    componentDidMount() {
+        return fetch('https://facebook.github.io/react-native/movies.json')
+        .then((response) => response.json())
+        .then((responseJson) => {
+
+            this.setState({
+            isLoading: false,
+            dataSource: responseJson.movies,
+            }, function(){
+
+            });
+            console.log("Hey Mother funcker!");
+        })
+        .catch((error) =>{
+            console.error(error);
+        });
+    }
+
     render() {
         const screenWidth = Dimensions.get('window').width;
         const chart_wh = screenWidth / 2;
@@ -22,19 +41,42 @@ class PieChartSummary extends Component {
             series.push(element.costAmount);
             colories.push(element.color);
         });
-
+        
         return(
-            <View>
-                <Text>pieChart</Text>
-                <PieChart
-                    chart_wh={chart_wh}
-                    series={series}
-                    sliceColor={colories}
-                />
+            <View style={styles.pieChartContainer}>
+                <View style={styles.titleBox}>
+                    <Text style={styles.titleStyle}>Current Cost View</Text>
+                </View>
+                <View style={styles.pieChartBox}>
+                    <PieChart
+                        chart_wh={chart_wh}
+                        series={series}
+                        sliceColor={colories}
+                    />
+                </View>
             </View>
         )
     }
 }
+
+let styles = StyleSheet.create({
+    pieChartContainer: {
+        flex: 10,
+        flexDirection: 'column'
+    },
+    titleBox: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems:'center'
+    },
+    pieChartBox: {
+        flex: 9
+    },
+    titleStyle: {
+        fontSize: 20
+    }
+});
+
 
 function mapStateToProps( state ) {
     return { costs: state.costs }
